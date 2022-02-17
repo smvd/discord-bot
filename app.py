7e23774331d1459185605ca0db109862088d0a3d
smvd <semvd06@gmail.com>
2022-02-17 17:30:21 +0100

    Update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from threading import Thread
-app = Flask('') 
+
+app = Flask('')
+
 @app.route('/')
 def main(): 
-    print("Your Bot Is Ready")
+    return "Your Bot Is Ready"
 
 def run(): 
     app.run(host="0.0.0.0", port=8000) 
